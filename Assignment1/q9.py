36eac7a515d8270dfeaf209a9ebf306a4887ff4a
Shiksha-pixel <mishra.9@iitj.ac.in>
2022-07-21 12:21:52 +0530

    new file

---
+++ Assignment1/q9.py
@@ -0,0 +1,7 @@
+'''q9. Write a program that accepts sequence of lines as input and prints the lines after making
+all characters in the sentence capitalized.
+'''
+str1=input("enter the string:  ")
+
+str1=str1.upper()
+print(str1)
