96a8a4383f6d4e5a7c611ea1138db848af75a53e
Shiksha-pixel <mishra.9@iitj.ac.in>
2022-08-04 11:54:46 +0530

    update new

---
+++ Assignment1/q23.py
@@ -0,0 +1,9 @@
+'''
+Write a method which can calculate square value of number
+'''
+
+ans=input("enter number for which square to be calculated :")
+
+ans=int(ans)
+
+print(ans**2)
---
+++ Assignment1/q25.py
@@ -0,0 +1,17 @@
+'''
+Define a class, which have a class parameter and have a same instance parameter.
+Hints: Define a instance parameter, need add itin init method
+You can init a object with construct parameter or set the valuelater
+'''
+
+class Object:
+    def __init_(self,val=10):
+        self.val=val
+
+    def assign(val):
+        self.val=val
+
+    def get():
+        return self.val
+
+        
