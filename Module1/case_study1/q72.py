a9d0e47e5b35c92e3e3f9b882b264c5c86c277c7
Shiksha-pixel <mishra.9@iitj.ac.in>
2022-08-30 17:32:50 +0530

    update

--- Module1/case_study1/q72.py
+++ Module1/case_study1/q72.py
@@ -1,1 +1,20 @@
-#hellow
+# 72. Please write a binary search function which searches an item in a sorted list. The
+# function should return the index of element to be searched in the list.
+# Hints: Use if/elif to deal with conditions.
+def binary_search(lst, item):
+    low = 0
+    high = len(lst) - 1
+    
+    while low <= high:
+        mid = round((low + high) / 2)
+        
+        if lst[mid] == item:
+            return mid
+        elif lst[mid] > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+    
+lst = [1,3,5,7,9,15,75,84,603,2114]
+print(binary_search(lst, 4)) 
